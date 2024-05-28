3cf8744642e334db3fe534b761771808fe5a947e
gonza <gonzalorom2001@gmail.com>
2024-05-28 00:40:32 -0300

    detalles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body : ['Lato', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
